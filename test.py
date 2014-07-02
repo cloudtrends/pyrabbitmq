20b4de3d6cef60fe2c6f8ca37d5163e9a4db948c
Your Name <you@example.com>
2014-07-02 09:08:20 +0800

    first rabbitmq test

---
+++ test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import pika
+
+
+print "ref http://www.rabbitmq.com/tutorials/tutorial-two-python.html"
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+               'localhost'))
+channel = connection.channel()
+
+
+
+channel.queue_declare(queue='hello')
+
+
+
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print " [x] Sent 'Hello World!'"
+
+
+
+
+connection.close()
